9ca67174aeca09dd41f7f86a9f306dd667a8993d
Joey Hills <joeymhills@gmail.com>
2024-06-10 13:55:25 -0400

    final commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,12 @@ module.exports = {
   content: ["./docs/**/*.{html,js}"],
   theme: {
     extend: {
+      fontFamily: {
+        copernicus: ['GalaxieCopernicus', 'sans-serif'],
+        polaris: ['GalaxiePolaris', 'sans-serif'],
+        polarisCondensed: ['GalaxiePolarisCondensed', 'sans-serif'],
+        // Add more fonts if necessary
+      },
       opacity: {
         '20': '0.2',
       },
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+    entry: './docs/animations.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'docs'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    },
+                },
+            },
+        ],
+    },
+    mode: 'development',
+};
